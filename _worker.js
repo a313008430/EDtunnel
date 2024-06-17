08214d92c8d45467c7c2726c846dae980402ff25
Mr.x <313008430@qq.com>
2024-06-17 22:51:01 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -15,7 +15,7 @@ let พร็อกซีไอพี = พร็อกซีไอพีs[Math.
 // let พร็อกซีไอพี = "[2a01:4f8:c2c:123f:64:5:6810:c55a]"
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
-
+console.log(env.UUID);
 if (!isValidUUID(userID)) {
 	throw new Error('uuid is invalid');
 }
